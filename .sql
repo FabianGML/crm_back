f505c906847155431d1162bc76dafee23d938fc0
Fabian Gutierrez <fabiangtml@gmail.com>
2023-11-14 20:34:24 -0600

    Login Endpoint response changed

--- .sql
+++ .sql
@@ -13,7 +13,9 @@ CREATE TABLE users (
 
 CREATE TABLE menu (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    business_entity VARCHAR(10) NOT NULL,
+    FOREIGN KEY (business_entity) REFERENCES users(business_entity)
 );
 
 
@@ -37,5 +39,21 @@ CREATE TABLE access (
 INSERT INTO users (username, email, business_entity, role, password) VALUES 
 ('admin', 'admin@admin.com', 'admin', 'admin', '$2b$15$9gSD0Cz8k6p.DwNmAY92MegqUDb9UGQ8/up2Qp3vkYCopljjM940G');
 
+INSERT INTO menu (name) VALUES
+('Administrar'),
+('Clientes')
+
+
+INSERT INTO options (name, url, menu_id) VALUES 
+('Crear', 'crear', 1),
+('Ventas', 'ventas', 1),
+('Ver Clientes', 'clientes', 2)
+('Crear Cliente', 'crear-cliente', 2)
+
+INSERT INTO access (user_id, option_id) VALUES 
+(UUID_TO_BIN('fa3c8bb1-7abb-11ee-8253-0242ac140002'), 1),
+(UUID_TO_BIN('fa3c8bb1-7abb-11ee-8253-0242ac140002'), 2),
+(UUID_TO_BIN('fa3c8bb1-7abb-11ee-8253-0242ac140002'), 3),
+(UUID_TO_BIN('fa3c8bb1-7abb-11ee-8253-0242ac140002'), 4)
 
 # admin's password: Admin123
--- models/Auth.js
+++ models/Auth.js
@@ -4,12 +4,28 @@ import signToken from '../utils/signToken.js'
 
 export class Auth {
   static async login ({ email, password }) {
-    const [user] = await connection.query('SELECT BIN_TO_UUID(id) AS id, password FROM users WHERE email = ?', [email])
+    const [user] = await connection.query('SELECT BIN_TO_UUID(id) AS id, password  FROM users WHERE email = ?', [email])
     const compare = await bcrypt.compare(password, user[0].password)
+    const menus = {}
     if (!compare) {
       return { error: 'Invalid email or password' }
     }
+    const [menuOptions] = await connection.query(
+      `
+      SELECT 
+        options.name AS options_name, 
+        options.url,
+        menu.name AS menu_id 
+      FROM access
+      JOIN options ON options.id = option_id
+      JOIN menu ON menu.id = menu_id
+      WHERE user_id = UUID_TO_BIN(?)
+      `, [user[0].id])
+    menuOptions.forEach(option => {
+      if (!menus[option.menu_id]) menus[option.menu_id] = []
+      menus[option.menu_id].push({ name: option.options_name, url: option.url })
+    })
     const token = signToken(user[0].id, 'service')
-    return { id: user[0].id, token }
+    return { id: user[0].id, token, menus }
   }
 }
